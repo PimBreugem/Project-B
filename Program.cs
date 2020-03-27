ea7233633df423c903d99b1558f76c37cf1ad762
PimBreugem <pim.breugem076@gmail.com>
2020-03-27 17:03:30 +0100

    V0.2

--- Program.cs
+++ Program.cs
@@ -4,22 +4,61 @@ namespace Project_B
 {
     class Program
     {
-        static void exit(){
-            Environment.Exit(0);
+        public static bool currentlylogged = false;
+        public static int currentId = 0;
+        public static Tuple<string,string>[] users = {
+                Tuple.Create("Admin", "12345"),
+                Tuple.Create("User", "54321")};
+        
+        static void login(){
+            Console.Clear();
+            Console.WriteLine("Please enter your username:");
+            string username = Console.ReadLine();
+            for(int i = 0; i < Program.users.Length; i++){
+                if(Program.users[i].Item1 == username){
+                    Console.Clear();
+                    Console.WriteLine("Please enter your password:");
+                    if(Program.users[i].Item2 == Console.ReadLine()){
+                        Program.currentId = i;
+                        Program.currentlylogged = true;
+                        Program.Main();
+                    }else{
+                        Console.Clear();
+                        Console.WriteLine("Invalid password");
+                        Program.Main();
+                    }
+                }else{
+                    Console.Clear();
+                    Console.WriteLine("Unknown username");
+                    Program.Main();
+                }
+            }
         }
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Reservation system");
+            Console.Clear();
+            if(Program.currentlylogged == true){
+                Console.WriteLine("Reservation system (logged as " + Program.users[Program.currentId].Item1 + ")\n");
+                Console.WriteLine("--Version // Shows version of program\n--Exit // Exit the program\n--Logout // logout");
+            }else{
+                Console.WriteLine("Reservation system\n");
+                Console.WriteLine("--Version // Shows version of program\n--Exit // Exit the program\n--Login // Sign in with an account");
+            }
             while(true){
                 string input = Console.ReadLine();
                 if (input == "exit" || input == "Exit"){
                     Environment.Exit(0);
                 }else if(input == "version" || input == "Version"){
-                    Console.WriteLine("Version 0.1");
-                }else if(input == "help" || input == "Help"){
-                    Console.WriteLine("--Version // Shows version of program");
-                    Console.WriteLine("--Exit // Exit the program");
-                    Console.WriteLine("--Help // Shows all the commands in the program");
+                    Console.WriteLine("Version 0.2");
+                }else if(input == "login" || input == "login"){
+                    Program.login();
+                }else if(input == "logout" || input =="Logout"){
+                    Program.currentlylogged = false;
+                    Program.Main();
+                }else if(input == "clear" || input =="Clear"){
+                    Program.Main();
+                }else{
+                    Program.Main();
                 }
             }
         }
