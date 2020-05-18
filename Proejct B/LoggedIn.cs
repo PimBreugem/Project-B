277662e948783f9c49663feceef77dcbe451821d
Tijmen <tijmen2063@gmail.com>
2020-05-18 17:52:22 +0200

    Windows forms Start
    Alle orginele bestanden zijn weg en vervangen voor windos forms start

---
+++ Proejct B/LoggedIn.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Proejct_B
+{
+    public partial class LoggedIn : Form
+    {
+        public LoggedIn()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
